312dd31315f97762106eb3437fb40b2d64ff4e40
daniel <ghub@limitedcreativity.org>
2007-08-01 20:52:33 +0000

    Fixed unit test runs
    git-svn-id: http://svn.talia.discovery-project.eu/talia/semantic_naming/trunk@63 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/predicate_test.rb
+++ test/predicate_test.rb
@@ -6,12 +6,12 @@ require File.dirname(__FILE__) + "/../lib/semantic_naming"
 class PredicateTest < Test::Unit::TestCase
   # Tests the basic registering of namespaces
   def test_shortcut
-    new_pr = N::Predicate.shortcut(:newpr, "http://www.new.com/")
+    new_pr = N::Predicate.shortcut(:newpr, "http://www.predicate_shortcut.com/")
     assert_equal(new_pr, N::NEWPR);
-    assert_equal(new_pr.to_s, "http://www.new.com/")
-    assert_equal(new_pr, N::URI.new("http://www.new.com/"))
+    assert_equal(new_pr.to_s, "http://www.predicate_shortcut.com/")
+    assert_equal(new_pr, N::URI.new("http://www.predicate_shortcut.com/"))
     assert_kind_of(N::Predicate, new_pr)
     assert_kind_of(N::Predicate, N::NEWPR)
-    assert_raise(NameError) { N::Namespace.shortcut(:newpr, "http://www.new.com/") }
+    assert_raise(NameError) { N::Namespace.shortcut(:newpr, "http://www.predicate_shortcut.com/") }
   end
 end
