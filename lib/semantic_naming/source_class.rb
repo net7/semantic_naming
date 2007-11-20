8f1f6c0a2257968ad60573249e1b76266aea07b5
daniel <ghub@limitedcreativity.org>
2007-11-20 12:14:22 +0000

    Updated for demo version
    git-svn-id: http://svn.talia.discovery-project.eu/talia/semantic_naming/trunk@209 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/semantic_naming/source_class.rb
+++ lib/semantic_naming/source_class.rb
@@ -8,7 +8,7 @@ module N
       qry = Query.new.distinct.select(:o)
       qry.where(Module::RDFS::Resource.new(@uri_s), Module::RDFS::Resource.new(RDFS::subClassOf.to_s), :o)
       qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new((RDFS + 'Class').to_s))
-      results = qry.execute.collect { |item| SourceClass.new(item.uri) }
+      qry.execute.collect { |item| SourceClass.new(item.uri) }
     end
     
     # Get the subtypes of this type
@@ -24,6 +24,13 @@ module N
       qry = Query.new.distinct.select(:s)
       qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new(@uri_s))
       qry.execute.collect { |item| TaliaCore::Source.new(item.uri) }
+    end
+    
+    # Get all the existing types from the RDF store
+    def self.rdf_types
+      qry = Query.new.distinct.select(:s)
+      qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new((RDFS + 'Class').to_s))
+      qry.execute.collect { |item| SourceClass.new(item.uri) }
     end
     
   end
