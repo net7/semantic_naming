5fb1847998dfc7c6fa8144cfd6d66f1802ccc3c9
daniel <daniel@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-01-17 14:06:52 +0000

    Small refactoring for the source_class
    git-svn-id: http://svn.talia.discovery-project.eu/talia/semantic_naming/trunk@322 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/semantic_naming/source_class.rb
+++ lib/semantic_naming/source_class.rb
@@ -1,36 +1,61 @@
 module N
 
-  # This is the type of URI that represents a class of sources
+  # This is the type of URI that represents a class of sources. The methods
+  # that browse the ontology hierarchy depend on ActiveRDF for accessing the
+  # RDF store. If ActiveRDF is not present, these will return nitl.
   class SourceClass < URI
     
     # Get the supertype of this class
     def supertypes
+      return nil unless(active_rdf?)
       qry = Query.new.distinct.select(:o)
-      qry.where(Module::RDFS::Resource.new(@uri_s), Module::RDFS::Resource.new(RDFS::subClassOf.to_s), :o)
-      qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new((RDFS + 'Class').to_s))
+      qry.where(Mmake_res(@uri_s), make_res(RDFS::subClassOf), :o)
+      qry.where(:o, make_res(RDF::type), make_res(RDFS + 'Class'))
       qry.execute.collect { |item| SourceClass.new(item.uri) }
     end
     
     # Get the subtypes of this type
     def subtypes
+      return nil unless(active_rdf?)
       qry = Query.new.distinct.select(:s)
-      qry.where(:s, Module::RDFS::Resource.new(RDFS::subClassOf.to_s), Module::RDFS::Resource.new(@uri_s))
-      qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new((RDFS + 'Class').to_s))
+      qry.where(:s, make_res(RDFS::subClassOf), make_res(@uri_s))
+      qry.where(:s, make_res(RDF::type), make_res(RDFS + 'Class'))
       qry.execute.collect { |item| SourceClass.new(item.uri) }
     end
     
     # Get the instances of this type
     def instances
+      return nil unless(active_rdf?)
       qry = Query.new.distinct.select(:s)
-      qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new(@uri_s))
+      qry.where(:s, make_res(RDF::type.to_s), make_res(@uri_s))
       qry.execute.collect { |item| TaliaCore::Source.new(item.uri) }
     end
     
     # Get all the existing types from the RDF store
     def self.rdf_types
+      return nil unless(active_rdf?)
       qry = Query.new.distinct.select(:s)
-      qry.where(:s, Module::RDFS::Resource.new(RDF::type.to_s), Module::RDFS::Resource.new((RDFS + 'Class').to_s))
+      qry.where(:s, make_res(RDF::type), make_res(RDFS + 'Class'))
       qry.execute.collect { |item| SourceClass.new(item.uri) }
+    end
+    
+    protected
+    
+    # Create a resource from the given type
+    def make_res(type)
+      Module::RDFS::Resource.new(type.to_s)
+    end
+    
+    @@active_rdf_checked = false
+    
+    # Check if the ActiveRDF library is present.
+    def active_rdf?
+      unless(@@active_rdf_checked)
+        @@active_rdf_present = defined?(Module::RDFS::Resource)
+        @@active_rdf_checked = true
+      end
+      
+      @@active_rdf_present
     end
     
   end
