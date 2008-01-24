ebf61e84cf08e622c1faec445876c2b6b5c2dac9
daniel <daniel@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-01-24 17:49:28 +0000

    Modifications and fixes for Aiwendil. Also translated the demo content for Lucca.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/semantic_naming/trunk@353 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/semantic_naming/source_class.rb
+++ lib/semantic_naming/source_class.rb
@@ -5,6 +5,9 @@ module N
   # RDF store. If ActiveRDF is not present, these will return nitl.
   class SourceClass < URI
     
+    # Match the language part in labels
+    @@lang_re = /(.*)@(.+)$/
+    
     # Get the supertype of this class
     def supertypes
       return nil unless(active_rdf?)
@@ -29,6 +32,19 @@ module N
       qry = Query.new.distinct.select(:s)
       qry.where(:s, make_res(RDF::type.to_s), make_res(@uri_s))
       qry.execute.collect { |item| TaliaCore::Source.new(item.uri) }
+    end
+    
+    # If the RDF store is active and the class has an rdfs:label, this will be
+    # returned. Otherwise, this will return the short name of the current source
+    # (like ns:name)
+    # 
+    # You may give a language, if none is given 'en' is used by default. If
+    # no label with a language marker is found, the first none-language-label
+    # is used
+    def label(language = 'en')
+      @label ||= label_by_lang(rdfs_labels, language)
+      # The rdf label is cache, while the default may change
+      return @label ? @label : to_name_s
     end
     
     # Get all the existing types from the RDF store
@@ -56,6 +72,33 @@ module N
       end
       
       @@active_rdf_present
+    end
+    
+    # Gets the rdfs:labels from the rdf store
+    def rdfs_labels
+      if(active_rdf?)
+        labels = make_res(@uri_s)[(N::RDFS::label).to_s]
+        if(labels && labels.size > 0)
+          labels
+        end # else nil
+      end # else nil
+    end
+    
+    # Gets the label for the given language. If no labels are given, return nil
+    def label_by_lang(labels, language = 'en')
+      return nil unless(labels)
+      result = nil
+      labels.each do |label|
+        if(match = @@lang_re.match(label)) # check if there is a "languag" string
+          if(match[2] == language)
+            return match[1] # We've found one, break here and return the label part
+          end
+        else
+          result ||= label # Keep the non-language label
+        end
+      end
+      
+      result
     end
     
   end
