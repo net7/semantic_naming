d747195ab79ddaa141471d14f95358f4feaf9181
gluca <guidi.luca@gmail.com>
2008-04-07 13:52:55 +0000

    Addind utility methods, required by RDF editing.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/semantic_naming@546 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/predicate_test.rb
+++ test/predicate_test.rb
@@ -4,14 +4,36 @@ require File.dirname(__FILE__) + "/../lib/semantic_naming"
   
 # Test the namespace functionality
 class PredicateTest < Test::Unit::TestCase
+  include N
+  def setup
+    @uri = "http://www.predicate_shortcut.com/"
+  end
+  
   # Tests the basic registering of namespaces
   def test_shortcut
-    new_pr = N::Predicate.shortcut(:newpr, "http://www.predicate_shortcut.com/")
-    assert_equal(new_pr, N::NEWPR);
-    assert_equal(new_pr.to_s, "http://www.predicate_shortcut.com/")
-    assert_equal(new_pr, N::URI.new("http://www.predicate_shortcut.com/"))
-    assert_kind_of(N::Predicate, new_pr)
-    assert_kind_of(N::Predicate, N::NEWPR)
-    assert_raise(NameError) { N::Namespace.shortcut(:newpr, "http://www.predicate_shortcut.com/") }
+    new_pr = Predicate.shortcut(:newpr, @uri)
+    assert_equal(new_pr, NEWPR);
+    assert_equal(new_pr.to_s, @uri)
+    assert_equal(new_pr, URI.new(@uri))
+    assert_kind_of(Predicate, new_pr)
+    assert_kind_of(Predicate, NEWPR)
+    assert_raise(NameError) { Namespace.shortcut(:newpr, @uri) }
+  end
+  
+  def test_should_return_local_name
+    assert_equal(['sources', '1'], predicate.send(:local_name))
+  end
+  
+  def test_should_return_namespace
+    assert_equal('sources', predicate.namespace)
+  end
+  
+  def test_should_return_name
+    assert_equal('1', predicate.name)
+  end
+  
+  private
+  def predicate
+    Predicate.new("#{@uri}sources#1")
   end
 end
