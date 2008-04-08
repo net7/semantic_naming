6195c1ac1de3394600b6e099be948416c620a2e8
gluca <guidi.luca@gmail.com>
2008-04-08 13:59:58 +0000

    Reverting [546].
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/semantic_naming@551 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/semantic_naming/predicate.rb
+++ lib/semantic_naming/predicate.rb
@@ -1,19 +1,6 @@
 module N
-
   # This is the type of URI that represents a type of predicate
   # (or relation, or property)
   class Predicate < URI
-    def namespace
-      @namespace ||= local_name.first
-    end
-    
-    def name
-      @name ||= local_name.last
-    end
-    
-    private
-    def local_name
-      to_s.split('/').last.split('#')
-    end
   end
 end
