92b7cf3c19ca5b5ae29354920396336af4207551
daniel <daniel@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-05-08 07:46:13 +0000

    Fixes/Modifications for JRuby
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/semantic_naming@624 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/semantic_naming/source_class.rb
+++ lib/semantic_naming/source_class.rb
@@ -7,7 +7,7 @@ module N
     
     # Get the supertype of this class
     def supertypes
-      return nil unless(active_rdf?)
+      return nil unless(active_rdf? && is_iri?)
       qry = Query.new.distinct.select(:o)
       qry.where(make_res(@uri_s), make_res(RDFS::subClassOf), :o)
       qry.where(:o, make_res(RDF::type), make_res(RDFS + 'Class'))
@@ -16,7 +16,7 @@ module N
     
     # Get the subtypes of this type
     def subtypes
-      return nil unless(active_rdf?)
+      return nil unless(active_rdf? && is_iri?)
       qry = Query.new.distinct.select(:s)
       qry.where(:s, make_res(RDFS::subClassOf), make_res(@uri_s))
       qry.where(:s, make_res(RDF::type), make_res(RDFS + 'Class'))
@@ -26,7 +26,7 @@ module N
     # Get the instances of this type. return_type will be the class used to 
     # create the objects that are returned.
     def instances(return_type)
-      return nil unless(active_rdf?)
+      return nil unless(active_rdf? && is_iri?)
       qry = Query.new.distinct.select(:s)
       qry.where(:s, make_res(RDF::type.to_s), make_res(@uri_s))
       qry.execute.collect { |item| return_type.new(item.uri) }
