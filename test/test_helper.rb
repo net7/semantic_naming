19796da4b1214946f9f60e88c637c177b45a48b4
daniel <daniel@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-05-17 10:05:20 +0000

    Better RDF adapter detection for tests.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/semantic_naming@647 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,26 @@ require File.dirname(__FILE__) + "/../lib/semantic_naming"
 begin
   $: << File.join(File.dirname(__FILE__), '..', '..', 'ActiveRDF', 'lib')
   require 'active_rdf'
-  RDF_ACTIVE = true
+  
+  adapter_found = true
+  if(ConnectionPool.adapter_types.include?(:sesame))
+    ConnectionPool.add(:type => :sesame, :location => 'test_sesame')
+    puts "Running tests with Sesame adapter."
+  elsif(ConnectionPool.adapter_types.include?(:redland))
+    ConnectionPool.add(:type => :redland, :location => 'hashes', :name => :test, :"hash-type" => :memory)
+    puts "Running tests with Redland adapter."
+  elsif(ConnectionPool.adapter_types.include?(:rdflite))
+    ConnectionPool.add(:type => :rdflite)
+    puts "Running tests with RDFLite adapter. Some may fail"
+  else
+    $stderr.puts "No suitable ActiveRDF adapter found. tests will not run."
+    adapter_found = false
+  end
+  
+  RDF_ACTIVE = adapter_found
 rescue Exception
   RDF_ACTIVE = false
-  $stderr.puts "Not running rdf-tests: ActiveRDF not found."
+  $stderr.puts "Not running rdf-tests: ActiveRDF could not be loaded."
 end
 
 # Check for the tesly adapter, and load it if it's there
@@ -18,8 +34,6 @@ N::Namespace.shortcut(:rdftest, 'http://rdftestdummy/')
 N::Namespace.shortcut(:rdftest2, 'http://rdftestdummy2/')
 
 if(RDF_ACTIVE)
-  # ConnectionPool.add(:type => :rdflite)
-  ConnectionPool.add(:type => :redland, :location => 'hashes', :name => :test, :"hash-type" => :memory)
   db = ConnectionPool.write_adapter
   db.add(N::RDFTEST.test1, N::RDF::type, N::RDFTEST.Type1)
   db.add(N::RDFTEST.test1, N::RDFS::label, "Like a virgin")
